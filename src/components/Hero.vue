969b38a92ffd5c53728450d53a793db60cdef115
Maykon Michel <maykonmichelpalma@gmail.com>
2018-09-01 19:21:05 -0300

    add Pagination

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,4 @@
 <template>
-  <b-card v-bind:title="hero.name"
-          v-bind:img-src="hero.thumbnail.path+'.'+hero.thumbnail.extension"
-          border-variant="danger"
-          img-alt="Image"
-          img-top
-          style="max-width: 14rem;"
-          class="mb-5">
-    <p class="card-text">
-    </p>
-    <b-button href="#" variant="danger">Mais detalhes</b-button>
-  </b-card>
 </template>
 
 <script>
@@ -20,12 +9,4 @@
 </script>
 
 <style scoped>
-  .card-img-top {
-    width: 222px;
-    height: 222px;
-  }
-
-  .card-title {
-    height: 2em;
-  }
 </style>
--- src/components/HeroesList.vue
+++ src/components/HeroesList.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
+    <b-pagination align="center" v-bind:total-rows="total" v-model="currentPage" :per-page="40"/>
     <b-card-group deck>
       <div v-for="hero of heroes">
-        <Hero v-bind:hero="hero"></Hero>
+        <b-card v-bind:title="hero.name"
+                v-bind:img-src="hero.thumbnail.path+'.'+hero.thumbnail.extension"
+                border-variant="danger"
+                img-alt="Image"
+                img-top
+                style="max-width: 14rem;"
+                class="mb-5">
+          <p class="card-text">
+            {{ hero.id }}
+          </p>
+          <b-button v-bind:href="'/heroes/'+hero.id" variant="danger">Mais detalhes</b-button>
+        </b-card>
       </div>
     </b-card-group>
-
-
+    <b-pagination align="center" v-bind:total-rows="total" v-model="currentPage" :per-page="40"/>
   </div>
 </template>
 
 <script>
-  import Hero from "./Hero";
-
   export default {
     name: 'heroes',
-    components: {Hero},
     data() {
       return {
-        heroes: []
+        heroes: [],
+        total: 0,
+        currentPage: 1
       }
     },
     methods: {
@@ -26,18 +36,34 @@
         this.apikey = '8b3f26486f67848fe413d5b2b415ca8d';
         this.ts = Math.round(+new Date() / 1000);
         this.hash = require('crypto').createHash('md5').update(this.ts + '8345a9ee9ac5194be045ce7465537cb0d43146a3' + this.apikey).digest('hex');
-        this.$http.get('https://gateway.marvel.com/v1/public/characters?apikey=' + this.apikey + '&ts=' + this.ts + '&hash=' + this.hash)
+        this.$http.get('https://gateway.marvel.com/v1/public/characters?apikey=' + this.apikey +
+          '&ts=' + this.ts + '&hash=' + this.hash + "&limit=40&offset=" + (this.currentPage-1) * 40)
           .then(response => {
+            this.total = response.data.data.total;
             this.heroes = response.data.data.results;
           });
       }
     },
+    watch: {
+      currentPage: function () {
+        this.updateSource();
+      }
+    },
     created: function () {
+      this.total = 0;
+      this.currentPage = 1;
       this.updateSource();
     }
   }
 </script>
 
 <style scoped>
+  .card-img-top {
+    width: 222px;
+    height: 222px;
+  }
 
+  .card-title {
+    height: 2em;
+  }
 </style>
