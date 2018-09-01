bd0ff324475db36e86236955bf219a6a8e5ff2e8
Maykon Michel <maykonmichelpalma@gmail.com>
2018-09-01 12:32:24 -0300

    add Navbar
    add favicon
    change page title

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="dark">
+    <b-container>
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="https://www.marvel.com/" target="_blank">
+        <h3><b-badge variant="danger">MARVEL</b-badge></h3>
+      </b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+          <b-nav-item href="#">Lista de heróis</b-nav-item>
+          <b-nav-item href="#">Sobre</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Pesquisar"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit" variant="danger">Pesquisar</b-button>
+          </b-nav-form>
+
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 
-/* eslint-disable no-new */
+Vue.config.productionTip = false;
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
+});
